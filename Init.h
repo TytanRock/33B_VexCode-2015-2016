daab98281b5f0fd1e6b94b10935e990519f507fb
coryness <coryness@ndpma.org>
2015-12-12 17:31:45 -0500

    Typedef enum 'right' fix
    There was a problem with a typedef, I think this should fix it.

--- Init.h
+++ Init.h
@@ -3,8 +3,8 @@
 
 #define LAUNCHER(value) motor(shooter1) = motor (shooter2) = (value)//Our shooter, 2 motors
 #define INTAKE(value) motor(intake1) = motor(intake2) = (value)			//Our collector, 2 motors
-#define LEFT_SIDE(value) motor(left) = (value)											//Our left side, 1 motor
-#define RIGHT_SIDE(value) motor(right) = (value)										//Our right side, 1 motor
+#define LEFT_SIDE(value) motor(left1) = (value)											//Our left side, 1 motor
+#define RIGHT_SIDE(value) motor(right1) = (value)										//Our right side, 1 motor
 
 void init();
 
